4453a4b207bf6529112e13faccabd141eea5de1d
Jeppe Wolff Johansen <jepper123411@proton.me>
2026-03-01 14:06:24 +0100

    Added vm

--- configuration.nix
+++ configuration.nix
@@ -1,117 +1,122 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
+config,
+lib,
+pkgs,
+...
 }: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+	imports = [
+		# Include the results of the hardware scan.
+		./hardware-configuration.nix
+	];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  services.getty.autologinUser = "z3co";
-  nixpkgs.config.allowUnfree = true;
+	networking.hostName = "nixos"; # Define your hostname.
+	services.getty.autologinUser = "z3co";
+	nixpkgs.config.allowUnfree = true;
 
-  networking.networkmanager.enable = true;
+	networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Copenhagen";
+	time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    keyMap = "dk";
-  };
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		keyMap = "dk";
+	};
 
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-  };
-  hardware.bluetooth.enable = true;
+	hardware.graphics.enable = true;
+	hardware.graphics.enable32Bit = true;
+	services.xserver.videoDrivers = ["nvidia"];
+	hardware.nvidia = {
+		modesetting.enable = true;
+		open = true;
+	};
+	hardware.bluetooth.enable = true;
+	programs.virt-manager.enable = true;
 
-  users.users.z3co = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "docker"];
-    initialPassword = "123";
-    shell = pkgs.zsh;
-  };
+	users.groups.libvirtd.members = ["z3co"];
+	virtualisation.libvirtd.enable = true;
+	virtualisation.spiceUSBRedirection.enable = true;
 
-  services.xserver.xkb.layout = "dk";
+	users.users.z3co = {
+		isNormalUser = true;
+		extraGroups = ["wheel" "docker"];
+		initialPassword = "123";
+		shell = pkgs.zsh;
+	};
 
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-  services.flatpak.enable = true;
-  services.kanata = {
-    enable = true;
-    keyboards.default.configFile = ./config/kanata/config.kbd;
-  };
-  services.twingate.enable = true;
+	services.xserver.xkb.layout = "dk";
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  programs.zsh.enable = true;
-  programs.neovim.defaultEditor = true;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
+	services.pipewire = {
+		enable = true;
+		pulse.enable = true;
+	};
+	services.flatpak.enable = true;
+	services.kanata = {
+		enable = true;
+		keyboards.default.configFile = ./config/kanata/config.kbd;
+	};
+	services.twingate.enable = true;
 
-  virtualisation.docker.enable = true;
+	programs.hyprland = {
+		enable = true;
+		xwayland.enable = true;
+	};
+	programs.zsh.enable = true;
+	programs.neovim.defaultEditor = true;
+	programs.steam.enable = true;
+	programs.steam.gamescopeSession.enable = true;
+	programs.gamemode.enable = true;
 
-  fonts.fontDir.enable = true;
+	virtualisation.docker.enable = true;
 
-  programs.gnupg.agent= {
-    enable = true;
-    enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-curses;
-  };
+	fonts.fontDir.enable = true;
 
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
-  };
-  security.pam.u2f = {
-    enable = true;
-    settings.cue = true;
-    settings.authfile = "/etc/secure-keys/u2f_keys";
-  };
-  services.pcscd.enable = true;
-  services.udev.packages = [ pkgs.yubikey-personalization ];
+	programs.gnupg.agent= {
+		enable = true;
+		enableSSHSupport = true;
+		pinentryPackage = pkgs.pinentry-curses;
+	};
 
-  environment.systemPackages = with pkgs; [
-    wget
-    kitty
-    fzf
-    zoxide
-    hyprpaper
-    hyprlock
-    hypridle
-    hyprshot
-    wofi
-    waybar
-    banana-cursor
-    mangohud
-    protonup-ng
-    swaynotificationcenter
-    pass
-    wl-clipboard
-    yubikey-manager
-    yubico-piv-tool
-    vicinae
-    just
-    pulseaudio
-  ];
+	security.pam.services = {
+		login.u2fAuth = true;
+		sudo.u2fAuth = true;
+	};
+	security.pam.u2f = {
+		enable = true;
+		settings.cue = true;
+		settings.authfile = "/etc/secure-keys/u2f_keys";
+	};
+	services.pcscd.enable = true;
+	services.udev.packages = [ pkgs.yubikey-personalization ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+	environment.systemPackages = with pkgs; [
+		wget
+		kitty
+		fzf
+		zoxide
+		hyprpaper
+		hyprlock
+		hypridle
+		hyprshot
+		wofi
+		waybar
+		banana-cursor
+		mangohud
+		protonup-ng
+		swaynotificationcenter
+		pass
+		wl-clipboard
+		yubikey-manager
+		yubico-piv-tool
+		vicinae
+		just
+		pulseaudio
+	];
 
-  system.stateVersion = "25.11"; # Did you read the comment?
+	nix.settings.experimental-features = ["nix-command" "flakes"];
+
+	system.stateVersion = "25.11"; # Did you read the comment?
 }
